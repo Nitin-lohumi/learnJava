00f0551f3c5baefc4ac86d8826e7cd0f4530749f
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-02-19 23:44:44 +0530

    java factorial of a number by user input

--- start.java
+++ start.java
@@ -1,26 +1,17 @@
-/**
- * start
- */
-import java.util.Scanner;
- public class start {
-
-    public static void main(String[] args) {
-        try (Scanner s1 = new Scanner(System.in)) {
-            System.out.println("enter the frist number");
-            
-            int row =s1.nextInt();
-            // System.out.println("enter the frist number");
-            // int col= s1.nextInt();?
-            int i;
-            int j;
-            for (i=1; i<=row; i++){
-                for(j=1; j<=i; j++){
-                    System.out.print(j +" ");
-                }
-                System.out.println(" ");
-            }
-
-          
-        }
-    }
-}
+import java.util.*;
+class start{  
+    public static void main(String args[]){  
+        int fact=1;  
+        @SuppressWarnings("resource")
+        Scanner s1 = new Scanner(System.in);
+        int number = s1.nextInt();//It is the number to calculate factorial    
+        fact = factorial(number);   
+        System.out.println("Factorial of "+number+" is: "+ fact);    
+    }  
+    static int factorial(int n){    
+     if (n == 0)    
+       return 1;    
+     else    
+       return(n * factorial(n-1));    
+    }    
+   }  
